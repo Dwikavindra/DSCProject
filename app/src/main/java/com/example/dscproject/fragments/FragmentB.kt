4bd738c6d613ec31683862f57780c5a467a76379
S0ULB0Y <dwikavindra.haryo.r@mail.ugm.ac.id>
2021-03-20 11:09:39 +0700

    SignUp and Sign In Features

--- app/src/main/java/com/example/dscproject/fragments/FragmentB.kt
+++ app/src/main/java/com/example/dscproject/fragments/FragmentB.kt
@@ -5,18 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.dscproject.R
+import com.example.dscproject.databinding.FragmentABinding
+import com.example.dscproject.databinding.FragmentBBinding
+import kotlinx.parcelize.Parcelize
 
 
 class FragmentB : Fragment() {
-
+    private var _binding: FragmentBBinding?=null
+    private val binding
+        get()=_binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_b, container, false)
+        _binding= FragmentBBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.movetofragmentc.setOnClickListener(){
+            val action=FragmentBDirections.actionFragmentBToFragmentC()
+            findNavController().navigate(action)
+        }
     }
 
 
--- app/src/main/java/com/example/dscproject/fragments/FragmentC.kt
+++ app/src/main/java/com/example/dscproject/fragments/FragmentC.kt
@@ -5,14 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.dscproject.R
+import com.example.dscproject.databinding.FragmentBBinding
+import com.example.dscproject.databinding.FragmentCBinding
 
 class FragmentC: Fragment() {
+    private var _binding: FragmentCBinding?=null
+    private val binding
+        get()=_binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_c, container, false)
+        _binding= FragmentCBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.movetoHomeSign.setOnClickListener(){
+            val action=FragmentCDirections.actionFragmentCToHomeSign()
+            findNavController().navigate(action)
+        }
+        binding.movetofragmentb.setOnClickListener(){
+            activity?.onBackPressed();
+        }
     }
 }
