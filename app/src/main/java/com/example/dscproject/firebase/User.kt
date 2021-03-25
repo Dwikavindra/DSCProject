0988cf7143d9c8f79ac0db3204c1f1c470a4be63
S0ULB0Y <dwikavindra.haryo.r@mail.ugm.ac.id>
2021-03-25 15:51:42 +0700

    recylerview for session completed

---
+++ app/src/main/java/com/example/dscproject/firebase/User.kt
@@ -0,0 +1,14 @@
+package com.example.dscproject
+
+class User (
+    val id:String=" ",
+            val firstname:String=" ",
+    val lastname:String=" ",
+    val email:String=" ",
+    val image:String= " ",
+    val mobile: Long =0,
+    val gender:String=" ",
+    val profileCompleted:Int=0,
+
+
+)
