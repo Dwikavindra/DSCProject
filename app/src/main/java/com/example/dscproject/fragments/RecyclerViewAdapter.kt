0d4e642409eb5e03ec2f7f1ec487e0c6e35cb144
S0ULB0Y <dwikavindra.haryo.r@mail.ugm.ac.id>
2021-04-18 13:50:59 +0700

    ThirdInit

--- app/src/main/java/com/example/dscproject/fragments/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/dscproject/fragments/RecyclerViewAdapter.kt
@@ -1,4 +1,49 @@
 package com.example.dscproject.fragments
 
-class RecyclerViewAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dscproject.R
+import com.example.dscproject.model.Sessions
+import org.w3c.dom.Text
+
+class RecyclerViewAdapter (private var sessions:List<Sessions>):
+    RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val session: TextView = itemView.findViewById(R.id.Sesi)
+        val waktu: TextView = itemView.findViewById(R.id.waktu)
+        val dokter1ruang: TextView = itemView.findViewById(R.id.Ruang2)
+        val dokter1nama: TextView = itemView.findViewById(R.id.namadokter2)
+        val dokter2ruang: TextView = itemView.findViewById(R.id.Ruang1)
+        val dokter2nama: TextView = itemView.findViewById(R.id.namadokter)
+        val dokter3ruang: TextView = itemView.findViewById(R.id.Ruang3)
+        val dokter3nama: TextView = itemView.findViewById(R.id.namadokter3)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val v=LayoutInflater.from(parent.context).inflate(R.layout.session_layout,parent,false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return sessions.size
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+      holder.session.text=sessions[position].sessions
+        holder.waktu.text=sessions[position].time
+        holder.dokter1ruang.text=sessions[position].idRuangan[0]
+        holder.dokter1nama.text=sessions[position].namadokter[0]
+        holder.dokter2ruang.text=sessions[position].idRuangan[1]
+        holder.dokter2nama.text=sessions[position].namadokter[1]
+        holder.dokter3ruang.text=sessions[position].idRuangan[2]
+        holder.dokter3nama.text=sessions[position].namadokter[2]
+
+    }
 }
+
+
